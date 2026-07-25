77ed854c6629c67e50b1892e4dd5e93d96dbe3a3
bmfp <bmfp.molin@gmail.com>
2026-07-25 23:59:06 +0200

    force managed python

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 cd "%~dp0"
 set "PATH=%cd%;%PATH%"
-start uv.exe run launcher.py
+start uv.exe --managed-python run launcher.py
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 cd $(dirname $0)
-PATH="$(pwd):$PATH" uv run "${1:-launcher.py}"
+PATH="$(pwd):$PATH" uv --managed-python run "${1:-launcher.py}"
 
